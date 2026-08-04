9a194982e98ce30cf081f36e8cd4c251f6b26836
ITFakeFace <145489024+ITFakeFace@users.noreply.github.com>
2026-08-04 10:02:35 +0700

    Delete Login 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import "./App.scss";
 import { Route, Routes } from "react-router-dom";
 import HomeScreen from "./Components/Screens/HomeScreen/HomeScreen";
 import Login from "./Components/Screens/Login/Login";
-import Login2 from "./Components/Screens/Login2/Login2";
 import Error404Screen from "./Components/Screens/Error404Screen/Error404Screen";
 import AuthLayout from "./Components/Layouts/AuthLayout/AuthLayout";
 import AdminLayout from "./Components/Layouts/AdminLayout/AdminLayout";
@@ -14,7 +13,6 @@ function App() {
       {/* Cụm route dành cho xác thực (Public) */}
       <Route path="/" element={<AuthLayout />}>
         <Route path="login" element={<Login />} />
-        <Route path="login2" element={<Login2 />} />
         <Route path="example" element={<ExampleScreen />} />
       </Route>
 
